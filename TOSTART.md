2cb6a1a72cbc2c50368e9945d89f97b43b099472
ajai-ss <ajai.srivastava@successive.tech>
2023-08-03 17:49:16 +0530

    Initial commit

---
+++ TOSTART.md
@@ -0,0 +1,6 @@
+# Run the following commands in sequence
+
+
+1. npm run build
+2. npm run dev
+3. npm run electron
---
+++ public/electron.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const { join } = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const url = isDev ? 'http://localhost:3000/' : `file://${join(__dirname, '../out/index.html')}`;
+  mainWindow.loadURL(url);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
